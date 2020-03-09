84f7b02b08b44bcf8709e1eb26b4cd0b6f00bb2b
orlandomendozagarrido <orlandomendozagarrido206@gmail.com>
2020-03-09 16:05:44 -0600

    Updates

---
+++ precache-manifest.18a5d9278e273948a60f61b117a636f6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "43b3ca160b78330af586",
+    "url": "/working/static/css/main.03ed7c95.chunk.css"
+  },
+  {
+    "revision": "43b3ca160b78330af586",
+    "url": "/working/static/js/main.d99bf087.chunk.js"
+  },
+  {
+    "revision": "9c9cc9f55d438a1e265b",
+    "url": "/working/static/js/runtime~main.1fea1eff.js"
+  },
+  {
+    "revision": "3e76410a0da7584d9443",
+    "url": "/working/static/js/2.2218ae77.chunk.js"
+  },
+  {
+    "revision": "84753208e3ace6c36a3a29c731983a69",
+    "url": "/working/static/media/slide_one.84753208.jpg"
+  },
+  {
+    "revision": "3a3ffd3696fea8999087bbdcf97d69d2",
+    "url": "/working/static/media/slide_two.3a3ffd36.jpg"
+  },
+  {
+    "revision": "9aa09a50b7eb84e96d522c10dfaa78e5",
+    "url": "/working/static/media/slide_three.9aa09a50.jpg"
+  },
+  {
+    "revision": "b78224b8be87d32fc33b2d5fbfc6124e",
+    "url": "/working/index.html"
+  }
+];
